ebc435163732e2f4baed938937f2591841be8313
j-0-n-e-z <j000neeez@gmail.com>
2023-03-31 00:58:56 +0500

    add TimeItem + styles

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,23 @@ import {
 	faInstagram,
 	faPinterest
 } from '@fortawesome/free-brands-svg-icons'
+import TimeItem from './TimeItem'
+import { pad } from './helpers'
 
-function App() {
+export default function App() {
 	const [time, setTime] = useState((8 * 24 + 23) * 60 * 60 + 55 * 60 + 41)
 
-	const getDays = () => ~~(time / 86400)
-	const getHours = () => ~~((time - getDays() * 86400) / 3600)
-	const getMinutes = () =>
-		~~((time - getDays() * 86400 - getHours() * 3600) / 60)
-	const getSeconds = () =>
-		~~(time - getDays() * 86400 - getHours() * 3600 - getMinutes() * 60)
+	const getDays = (time: number) => ~~(time / 86400)
+	const getHours = (time: number) => ~~((time - getDays(time) * 86400) / 3600)
+	const getMinutes = (time: number) =>
+		~~((time - getDays(time) * 86400 - getHours(time) * 3600) / 60)
+	const getSeconds = (time: number) =>
+		~~(
+			time -
+			getDays(time) * 86400 -
+			getHours(time) * 3600 -
+			getMinutes(time) * 60
+		)
 
 	useEffect(() => {
 		const interval = setInterval(() => {
@@ -29,95 +36,44 @@ function App() {
 		}
 	}, [])
 
-	const pad = (num: number) => num.toString().padStart(2, '0')
+	const timeItems: Record<string, () => number> = {
+		days: () => getDays(time),
+		hours: () => getHours(time),
+		minutes: () => getMinutes(time),
+		seconds: () => getSeconds(time)
+	}
 
 	return (
 		<div className='grid content-start justify-center justify-items-center h-screen bg-veryDark'>
-			<img className='absolute top-0 left-0' src={bgStars} alt='stars' />
-			<img
-				className='absolute bottom-0 left-[50%] translate-x-[-50%]'
-				src={patternHills}
-				alt='hills'
-			/>
-			<h1 className='text-white uppercase text-xl tracking-[.4rem] mt-40'>
-				we're launching soon
-			</h1>
-			<div className='grid grid-flow-col gap-8 mt-28'>
-				<div className='flex flex-col gap-5 items-center'>
-					<div className='timer-item'>
-						<div className='z-10 w-2 h-3 rounded-[0_100%_100%_0] bg-veryDark'></div>
-						<div className='z-10 text-[80px] text-softRed'>
-							{pad(getDays())}
-						</div>
-						<div className='z-10 w-2 h-3 rounded-[100%_0_0_100%] bg-veryDark'></div>
-						<div className='absolute z-0 w-full h-[1px] bg-veryDarkBlue'></div>
-					</div>
-					<div className='text-grayBlue text-sm tracking-[.3rem] uppercase'>
-						days
-					</div>
-				</div>
-				<div className='flex flex-col gap-5 items-center'>
-					<div className='timer-item'>
-						<div className='z-10 w-2 h-3 rounded-[0_100%_100%_0] bg-veryDark'></div>
-						<div className='z-10 text-[80px] text-softRed'>
-							{pad(getHours())}
-						</div>
-						<div className='z-10 w-2 h-3 rounded-[100%_0_0_100%] bg-veryDark'></div>
-						<div className='absolute z-0 w-full h-[1px] bg-veryDarkBlue'></div>
-					</div>
-					<div className='text-grayBlue text-sm tracking-[.3rem] uppercase'>
-						hours
-					</div>
-				</div>
-				<div className='flex flex-col gap-5 items-center'>
-					<div className='timer-item'>
-						<div className='z-10 w-2 h-3 rounded-[0_100%_100%_0] bg-veryDark'></div>
-						<div className='z-10 text-[80px] text-softRed'>
-							{pad(getMinutes())}
-						</div>
-						<div className='z-10 w-2 h-3 rounded-[100%_0_0_100%] bg-veryDark'></div>
-						<div className='absolute z-0 w-full h-[1px] bg-veryDarkBlue'></div>
-					</div>
-					<div className='text-grayBlue text-sm tracking-[.3rem] uppercase'>
-						minutes
-					</div>
-				</div>
-				<div className='flex flex-col gap-5 items-center'>
-					<div className='timer-item'>
-						<div className='w-2 h-3 rounded-[0_100%_100%_0] bg-veryDark z-10'></div>
-						<div className='text-[80px] text-softRed z-10'>
-							{pad(getSeconds())}
-						</div>
-						<div className='w-2 h-3 rounded-[100%_0_0_100%] bg-veryDark z-10'></div>
-						<div className='absolute z-0 w-full h-[1px] bg-veryDarkBlue'></div>
-					</div>
-					<div className='text-grayBlue text-sm tracking-[.3rem] uppercase'>
-						seconds
-					</div>
-				</div>
+			<div className='absolute top-0 left-[50%] translate-x-[-50%] w-full flex justify-center'>
+				<img src={bgStars} alt='stars' />
 			</div>
-			<div className='flex gap-10 mt-[350px] z-10'>
-				<a
-					href='#'
-					className='cursor-pointer text-grayBlue hover:text-white text-3xl'
-				>
+			<div className='absolute bottom-0 left-[50%] translate-x-[-50%] w-full flex justify-center'>
+				<img src={patternHills} alt='hills' />
+			</div>
+			<h1 className='text-white uppercase lg:text-2xl lg-text-lg tracking-[.4rem] lg:mt-40 mt-36 lg:w-full w-80 text-center'>
+				we’re launching soon
+			</h1>
+			<div className='grid grid-flow-col lg:gap-8 gap-2 lg:mt-28 mt-16'>
+				{Object.keys(timeItems).map(timeItem => (
+					<TimeItem
+						key={timeItem}
+						timeItem={timeItem}
+						getTimeItem={timeItems[timeItem]}
+					/>
+				))}
+			</div>
+			<div className='absolute lg:bottom-16 bottom-10 flex gap-10 z-10'>
+				<a href='#' className='brand-link'>
 					<FontAwesomeIcon icon={faFacebookSquare} />
 				</a>
-				<a
-					href='#'
-					className='cursor-pointer text-grayBlue hover:text-white text-3xl'
-				>
+				<a href='#' className='brand-link'>
 					<FontAwesomeIcon icon={faPinterest} />
 				</a>
-				<a
-					href='#'
-					className='cursor-pointer text-grayBlue hover:text-white text-3xl'
-				>
+				<a href='#' className='brand-link'>
 					<FontAwesomeIcon icon={faInstagram} />
 				</a>
 			</div>
 		</div>
 	)
 }
-
-export default App
---
+++ src/helpers.ts
@@ -0,0 +1,1 @@
+export const pad = (num: number) => num.toString().padStart(2, '0')
